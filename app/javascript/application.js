dfb6e31d4b9945333c35cbfeff05320e6997c88e
Paul Shippy <paul.shippy@strongmind.com>
2025-04-16 22:40:08 -0700

    Define callback function early

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+// Define the Spotify callback function in the global scope
+// This MUST be defined before the Spotify SDK loads
+window.onSpotifyWebPlaybackSDKReady = function() {
+    console.log('Spotify Web Playback SDK Ready')
+    document.dispatchEvent(new Event('spotify:sdk:ready'))
+  }
